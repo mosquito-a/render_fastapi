fc6e59dca7ede11249ebcf5ce4d924725ad0f2c6
mosquito-a <yanagawa010127@gmail.com>
2025-06-19 23:34:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from typing import Optional
-
+# main.py
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+@app.get("/index", response_class=HTMLResponse)
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>私の素晴らしいページ</title>
+        </head>
+        <body>
+            <h1>ようこそ、私の最初のウェブページへ！</h1>
+            <p>これはカスタムコンテンツです。</p>
+            <ul>
+                <li>アイテム1</li>
+                <li>アイテム2</li>
+            </ul>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/receive_message")
+async def receive_message(message: str):
+    return {"server_response": f"メッセージを受け取りました！: '{message}'。ありがとうございます！"}
